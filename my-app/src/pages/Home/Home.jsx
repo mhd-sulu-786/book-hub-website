49c631e101382a15a2ea578537f8244736078a7d
Aghoshs <aghoshsuresh5@gmail.com>
2024-11-17 19:41:34 +0530

    final file

---
+++ my-app/src/pages/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../../components/Header/Header';
+
+import { outlet } from 'react-router-dom';
+
+const Home = () => {
+  return (
+    <main>
+      <Header />
+      <outlet />
+
+    </main>
+  )
+}
+
+export default Home
